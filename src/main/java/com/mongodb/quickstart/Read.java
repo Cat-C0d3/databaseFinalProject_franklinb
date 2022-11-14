8ee97158d96b415e273792088a13fb2aab886197
Bailey Franklin <41973188+ThatCellist@users.noreply.github.com>
2022-11-14 11:38:51 -0600

    added queries

--- src/main/java/com/mongodb/quickstart/Read.java
+++ src/main/java/com/mongodb/quickstart/Read.java
@@ -2,15 +2,11 @@ package com.mongodb.quickstart;
 
 import com.mongodb.client.*;
 import com.mongodb.client.model.*;
-import com.mongodb.quickstart.models.Category;
-import org.bson.BSONObject;
-import org.bson.BsonDocument;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.mongodb.client.model.Filters.and;
 
@@ -26,8 +22,8 @@ public class Read {
 
             getNumVideosInCategories(videoCollection, categoryCollection);
             getNumVideosInCategoriesWithNonZeroInventory(videoCollection, categoryCollection);
-//            getCategoriesForActors(actorCollection, categoryCollection, videoCollection);
-//            getActorsWithMultipleCategories(actorCollection, categoryCollection, videoCollection);
+            getCategoriesForActors(actorCollection, categoryCollection, videoCollection);
+            getActorsWithMultipleCategories(actorCollection, categoryCollection, videoCollection);
 //            getNonComedyActors(actorCollection, categoryCollection, videoCollection);
 //            getComedyAndActionActors(actorCollection, categoryCollection, videoCollection);
         }
@@ -47,29 +43,55 @@ public class Read {
         //get videos in category
         //check inventory from last result
         FindIterable<Document> categories = categoryCollection.find();
+        int numPerCategory;
         categories.forEach(x ->{
             Bson filter = and(Filters.eq("category", x.get("name")), Filters.nin("stock_count", 0));
-            getVideosFromCategory(videoCollection, x, filter);
+            System.out.println(x.get("name") + " " + getVideosFromCategory(videoCollection, x, filter));
         });
 
     }
 
-    private static void getVideosFromCategory(MongoCollection videoCollection, Document x, Bson filter) {
+    private static int getVideosFromCategory(MongoCollection videoCollection, Document x, Bson filter) {
         int numPerCategory = 0;
         FindIterable<Document> videos = videoCollection.find(filter);
         Iterator videoIterator = videos.iterator();
         while (videoIterator.hasNext()){
             numPerCategory++;
         }
-        System.out.println(x.get("name") + " " + numPerCategory);
+        return numPerCategory;
     }
 
     public static void getCategoriesForActors(MongoCollection actorCollection, MongoCollection categoryCollection, MongoCollection videoCollection) {
+        //extract video ids from actor
+        //use video ids to get video collection
+        //count collection by categories
+        FindIterable<Document> categories = categoryCollection.find();
+        FindIterable<Document> actors = actorCollection.find();
 
+        actors.forEach(x -> {
+            System.out.println(x.get("name"));
+            categories.forEach(y -> {
+                Bson filter = and(Filters.eq("category", x.get("name")), Filters.eq("recording_id", x.get("recording_id")));
+                System.out.println(x.get("name") + " " + getVideosFromCategory(videoCollection, x, filter));
+            });
+        });
     }
 
     public static void getActorsWithMultipleCategories(MongoCollection actorCollection, MongoCollection categoryCollection, MongoCollection videoCollection) {
-
+        FindIterable<Document> categories = categoryCollection.find();
+        FindIterable<Document> actors = actorCollection.find();
+        AtomicInteger numCategories = new AtomicInteger();
+        actors.forEach(x -> {
+            System.out.println(x.get("name"));
+            categories.forEach(y -> {
+                Bson filter = and(Filters.eq("category", x.get("name")), Filters.eq("recording_id", x.get("recording_id")));
+                int numPerCategory = getVideosFromCategory(videoCollection, x, filter);
+                if(numPerCategory > 0 ){
+                    numCategories.getAndIncrement();
+                }
+            });
+            if (numCategories.get() > 0) System.out.println(x.get("name") + " " + numCategories);
+        });
     }
 
     public static void getNonComedyActors(MongoCollection actorCollection, MongoCollection categoryCollection, MongoCollection videoCollection) {
